727e576f950b8860d6c72d25f5e7e4caec222c93
ValeMoreno177 <valeriamorenofuentes529@gmail.com>
2025-07-03 11:08:45 -0600

    Starting with JFrame

--- src/App.java
+++ src/App.java
@@ -2,12 +2,10 @@ import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       JFrame = frama = new JFrame("App Java Swing");
-        frame.setSize(500,500)
+        JFrame frame = new JFrame("App Java Swing");
+        frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        
-
 
     }
 }
